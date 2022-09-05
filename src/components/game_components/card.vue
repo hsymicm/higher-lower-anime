923391f1671d76db684b99617e9166dcf35b7135
hsymicm <ichorvfx@gmail.com>
2022-09-05 19:47:05 +0700

    Change `getColor()` function to return HSL values

--- src/components/game_components/card.vue
+++ src/components/game_components/card.vue
@@ -36,19 +36,35 @@ export default {
             return string.length > length ? string.substring(0, length - 2) + ".." : string
         },
         getColor(hex) {
+            // IF UNDEFINED RETURN WHITE
             if(!hex) return '#fff'
-            
-            const limit = 180
-            let rgb = parseInt(hex.substring(1) , 16)   // convert rrggbb to decimal
-            
-            let r = (rgb >> 16) & 0xff  // extract red
-            let g = (rgb >>  8) & 0xff  // extract green
-            let b = (rgb >>  0) & 0xff  // extract blue
 
-            let luma = 0.2126 * r + 0.7152 * g + 0.0722 * b // per ITU-R BT.709 (0..225)
-            const lighten = luma > limit ? 1 : 1 + (limit - luma) / limit * 2
+            // HEX TO DECIMAL
+            let rgb = parseInt(hex.substring(1) , 16)
+            
+            // CONVERT TO RGB
+            let r = ((rgb >> 16) & 0xff) / 255
+            let g = ((rgb >>  8) & 0xff) / 255
+            let b = ((rgb >>  0) & 0xff) / 255
+            
+            // EXTRACT HUE
+            let max = Math.max(r, g, b)
+            let min = Math.min(r, g, b)
+            let h = (max + min) / 2
 
-            return `rgb(${r * lighten} ${g * lighten} ${b * lighten})`
+            if(max == min){
+                // ACHROMATIC
+                h = 0
+            }else{
+                let d = max - min;
+                switch(max){
+                    case r: h = (g - b) / d + (g < b ? 6 : 0); break
+                    case g: h = (b - r) / d + 2; break
+                    case b: h = (r - g) / d + 4; break
+                }
+                h /= 6
+            }
+            return `hsl(${Math.round(360*h)}, ${90}%, ${75}%)`
         }
     },
     components :{
