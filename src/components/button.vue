5acbfe058482d58786b6b58df904405f90a4156f
hsymicm <ichorvfx@gmail.com>
2022-09-05 23:35:18 +0700

    Add glow

--- src/components/button.vue
+++ src/components/button.vue
@@ -1,6 +1,6 @@
 <template>
     <button :class="[isHigherLower ? 'higherlower-button' : (isSecondary ? 'secondary-button' : 'primary-button')]">
-        {{ label }}<font-awesome-icon :class="[isHigher ? 'text-green ' : 'text-red', isHigherLower ? '' : 'hidden', 'ml-4']" :icon="isHigher? 'caret-up' : 'caret-down'"  :transform="isHigher? 'down-1' : 'up-1'" size="xl" />
+        {{ label }}<font-awesome-icon :class="[isHigher ? 'text-green' : 'text-red', isHigherLower ? '' : 'hidden', 'ml-4']" :icon="isHigher? 'caret-up' : 'caret-down'"  :transform="isHigher? 'down-1' : 'up-1'" size="xl" />
     </button>
 </template>
 
