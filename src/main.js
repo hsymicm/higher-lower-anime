3a16b31926be749186ee60c2669c7a1c887af56f
hsymicm <ichorvfx@gmail.com>
2022-09-24 01:25:13 +0700

    Added new logo

--- src/main.js
+++ src/main.js
@@ -9,9 +9,31 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faCaretDown, faCaretUp, faCaretLeft, faCaretRight, faCheck, faXmark } from '@fortawesome/free-solid-svg-icons'
+import 
+{ 
+    faCaretDown, 
+    faCaretUp, 
+    faCaretLeft, 
+    faCaretRight, 
+    faCheck, 
+    faXmark, 
+    faShare, 
+    faGlobe,
+    faGamepad,
+} 
+from '@fortawesome/free-solid-svg-icons'
 
-library.add(faCaretDown, faCaretUp, faCaretLeft, faCaretRight, faCheck, faXmark)
+library.add(
+    faCaretDown, 
+    faCaretUp, 
+    faCaretLeft, 
+    faCaretRight, 
+    faCheck, 
+    faXmark, 
+    faShare, 
+    faGlobe,
+    faGamepad,
+)
 
 /* add font awesome icon component */
 createApp(App)
