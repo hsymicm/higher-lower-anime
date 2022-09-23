99f92fe41e598f2429cb9822f6ad895a4e9e7e63
hsymicm <ichorvfx@gmail.com>
2022-09-24 01:26:13 +0700

    Create new card button  component

---
+++ src/components/game_components/cardButton.vue
@@ -0,0 +1,17 @@
+<template>
+    <button class="higherlower-button">
+        {{ label }}<font-awesome-icon :class="[isHigher ? 'text-green' : 'text-red', 'ml-4']" :icon="isHigher? 'caret-up' : 'caret-down'"  :transform="isHigher? 'down-1' : 'up-1'" size="xl" />
+    </button>
+</template>
+
+<script>
+export default {
+    props: {
+        label : {
+            type : String,
+            default : "Text",
+        },
+        isHigher : Boolean,
+    }
+}
+</script>
